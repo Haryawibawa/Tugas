31fded500e8dce6656047f134c45138505547e9c
haryawibawa <haryawibawaa@gmail.com>
2021-09-24 10:25:35 +0700

    HelloWorld.java

---
+++ Praktikum Algoritma/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+	public static void main (String [] args)  {
+		System.out.println ("Nama: Harya Wibawa");
+		System.out.println ("Nim : 6706213106");
+		System.out.println ("Kelas : D3RPLA 45-05");
+	}
+}
